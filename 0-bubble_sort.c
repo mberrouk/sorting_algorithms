7361f45163e8d82e388521e359a6f93e2273d71e
mberrouk <mberrouk@student.1337.ma>
2023-09-29 13:49:10 -0700

    INIT_1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,13 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - sorts an array of integers in ascending
+ *				order using the Bubble sort algorithm
+ * @array: array of int
+ * @size: size of array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
-		return ;
+		return;
 }
